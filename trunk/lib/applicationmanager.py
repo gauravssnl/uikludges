bf62056ffdeff572d3d35762b626aeb3b45cd5f6
mikko@redinnovation.com <mikko@redinnovation.com@f00fa536-ab46-0410-9f92-13116168796a>
2008-06-03 12:25:57 +0000

    Added background launcher functionality

--- trunk/lib/applicationmanager.py
+++ trunk/lib/applicationmanager.py
@@ -1,6 +1,6 @@
 """
 
-	Application manager allows programmaticalyl manipalte Series 60 applications.
+	Application manager allows programmatically manipulate Series 60 applications.
 				
 	
 """
@@ -11,7 +11,8 @@ __author__ = "Mikko Ohtamaa <mikko@redinnovation.com>"
 __copyright__ = "Copyright 2008 Red Innovation Ltd."
 __docformat__ = "epytext"
 __license__ = "BSD" 
-__version__ = "2.0"
+
+__version__ = "2.1"
 
 import e32
 
@@ -67,22 +68,51 @@ def end_app(appname):
 	"""	
 	return _applicationmanager.end_app(appname)
 
+def launch_py_background(python_file, launcher=u"uikludges_python_launcher.exe"):
+	""" Launch Python as a background process.
+	
+	This function works around the limitations of e32.start_server process
+	which has zero capabilities for the launched EXE.::
+	
+		ensymble.py altere32 modules\applicationmanager\uikludges_python_launcher.exe --caps=PowerMgmt+ReadDeviceData+WriteDeviceData+TrustedUI+ProtServ+SwEvent+NetworkServices+LocalServices+ReadUserData+WriteUserData+Location+SurroundingsDD+UserEnvironment --uid=0xA0008CDB  modules\applicationmanager\uikludges_python_launcher_mod.exe
+	
+	The default launcher is built against Series 60 3.0 FP1 and
+	PyS60 1.4.3. If you use other configuration you might need
+	to recompile python_launcher.exe to match your Application
+	Binary Interface. The application EXE drop is in applicationmanager
+	folder.
+	
+	The default launcher is enabled with all dev. cert. cabilities.
+	Use Ensymble tool to tune capabilities for Symbian Signed process.
+	You need to rewrite the UID also.
+	
+	@param python_file: Full path to Python file in Symbian path convention
+	@type python_file: unicode
+	
+	@param launcher: Stub EXE used to execute the file
+	@type launcher: unicode
+	
+	@return: None
+	@raise SymbianError: If the file does not exist or capability problems
+	"""	
+	return _applicationmanager.launch_py_background(appname)
+
+
 #: Exported functions from this module
 __all__ = ["switch_to_bg", 
 		   "switch_to_fg", 
 		   "kill_app", 
 		   "end_app", 
-		   "application_list"]
+		   "application_list",
+		   "launch_py_background"]
 
-# Export all E*** pseudo constants automatically
 def _test():
 	""" Self-testing for uikludges module. 
 		
 	"""
 	
 	print str(application_list())
-	
-		
+			
 if __name__ == "__main__":
 	# Perform self test is this Python module is used as an application entry point	
 	_test()
